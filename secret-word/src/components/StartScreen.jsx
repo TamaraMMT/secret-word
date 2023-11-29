09506e6353626b92a3c74f479ca84b39b9d5eaa0
TamaraMMT <tamaramolinast@gmail.com>
2023-11-29 19:11:46 -0300

    home screen, title, button  -  styles

---
+++ secret-word/src/components/StartScreen.jsx
@@ -0,0 +1,12 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+      <h1>Secret Word</h1>
+      <p>Click the button to come play</p>
+      <button>Play!</button>
+    </div>
+  )
+}
+export default StartScreen
