f6dc501ee0bc49d0bb28f965ad0c198471a10676
TamaraMMT <tamaramolinast@gmail.com>
2023-11-29 19:07:42 -0300

    data for game

---
+++ secret-word/src/data/words.jsx
@@ -0,0 +1,8 @@
+export const WordsList = {
+  car: ["Car", "Radio", "Brake", "Tire", "Accelerator"],
+  fruit: ["Banana", "Apple", "Pear", "Papaya", "Orange"],
+  body: ["Arm", "Leg", "Brain", "Neck", "Eyes"],
+  computer: ["Mouse", "Keyboard", "Monitor", "Case"],
+  programming: ["Language", "Framework", "JavaScript", "React"],
+  food: ["Rice", "Beans", "Meat", "Milk", "Egg"],
+};
