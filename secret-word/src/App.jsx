32549aee7fe1c7f38584f4d51f153643dde714d3
TamaraMMT <tamaramolinast@gmail.com>
2023-12-01 19:01:30 -0300

    reset values, guesses and score

--- secret-word/src/App.jsx
+++ secret-word/src/App.jsx
@@ -18,6 +18,8 @@ const stages = [
   { id: 3, name: "end" },
 ];
 
+const guessesQty = 5;
+
 function App() {
   const [gameState, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
@@ -28,7 +30,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const pickWordandCategory = () => {
@@ -81,10 +83,21 @@ function App() {
     }
   };
 
-  console.log(wrongLetters);
-
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  };
+  useEffect(() => {
+    if (guesses <= 0) {
+      clearLetterStates();
+      setGameStage(stages[2].name);
+    }
+  }, [guesses]);
 
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
     setGameStage(stages[0].name);
   };
 
@@ -105,7 +118,7 @@ function App() {
               score={score}
             />
           )}
-          {gameState === "end" && <GameOver retry={retry} />}
+          {gameState === "end" && <GameOver retry={retry} score={score} />}
         </div>
       </div>
     </>
