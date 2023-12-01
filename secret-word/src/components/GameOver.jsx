be43c680a0e25620ebb649a70c5f23813be8beb6
TamaraMMT <tamaramolinast@gmail.com>
2023-12-01 19:05:20 -0300

    added score

--- secret-word/src/components/GameOver.jsx
+++ secret-word/src/components/GameOver.jsx
@@ -2,10 +2,13 @@ import PropTypes from "prop-types";
 
 import "./GameOver.css";
 
-const GameOver = ({ retry }) => {
+const GameOver = ({ retry, score }) => {
   return (
     <div>
       <h1>Game Over</h1>
+      <h2>
+        Your score: <span className="score-end-game">{score}</span>
+      </h2>
       <button onClick={retry}>Reset</button>
     </div>
   );
