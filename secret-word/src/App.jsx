0ac867c67abc1ea558fcbe5274e5bde5f5c77291
TamaraMMT <tamaramolinast@gmail.com>
2023-12-01 18:06:52 -0300

    added letters in guessedLetters and wrongLetters

--- secret-word/src/App.jsx
+++ secret-word/src/App.jsx
@@ -57,8 +57,32 @@ function App() {
   };
 
   const verifyLetter = (letter) => {
-    console.log(letter);
+    const normalizedLetter = letter.toLowerCase();
+
+    if (
+      guessedLetters.includes(normalizedLetter) ||
+      wrongLetters.includes(normalizedLetter)
+    ) {
+      return;
+    }
+
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualGuessedLetters) => [
+        ...actualGuessedLetters,
+        normalizedLetter,
+      ]);
+    } else {
+      setWrongLetters((actualWrongLetters) => [
+        ...actualWrongLetters,
+        normalizedLetter,
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
   };
+
+  console.log(wrongLetters);
+
 
   const retry = () => {
     setGameStage(stages[0].name);
